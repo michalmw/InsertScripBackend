d916df11e0af9be4f97ada6a484517e4a26defa7
Robert Mrowiec <rmrowiec@icloud.com>
2018-02-09 19:32:40 +0100

    handle change edit

--- handleConnection.js
+++ handleConnection.js
@@ -1,14 +1,21 @@
+
+
 function connect(ws, req) {
+    ws.send('Hi there, I am a WebSocket server');
+
     console.log(req.headers.cookie);
     console.log('Client connected');
 
-    ws.on('request', req => {
+    ws.on('open', req => {
+      console.log('open')
+    })
 
+    ws.on('request', req => {
     })
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message)
-        ws.send(JSON.parse('test'))
+        ws.send(message)
     })
 
     ws.on('close', () => console.log('Client disconnected'));
