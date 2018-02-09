50413f6d6bc0e1bfbbccc72e46a0e176706db3d1
Robert Mrowiec <rmrowiec@icloud.com>
2018-02-09 14:53:24 +0100

    added package.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const Koa = require('koa')
+const Koa = require('koa');
+const router = require('koa-router')();
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8070 });
 
-const router = require('koa-router')()
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
 
 router.get('/hello', ctx => {
     ctx.body = {
         Hello: 'World'
-    }
-})
+    };
+});
 
-const app = new Koa()
-app.use(require('koa-bodyparser')())
+const app = new Koa();
+app.use(require('koa-bodyparser')());
 
-app.use(router.routes())
+app.use(router.routes());
 
-app.listen(8080)
+app.listen(8080);
