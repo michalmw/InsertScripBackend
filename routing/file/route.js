9ddb7f9d12b992639504139f53c4ee34d6576193
Griffith <jangrabara@gmail.com>
2018-02-10 08:37:27 +0100

    ctx

--- routing/file/route.js
+++ routing/file/route.js
@@ -6,16 +6,16 @@ function createSaveOrder() {
     return async (ctx) => {
 
         let message = await Message.findOne({ sessionId: ctx.session.id }).lean().exec()
-        await fs.writeFile(`../../upload/${ctx.body.name}.img`, new Buffer(request.body.content, "base64"))
+        await fs.writeFile(`../../upload/${ctx.body.name}.img`, new Buffer(ctx.request.body.content, "base64"))
         
         if (!message) throw 'Canot add file before first message'
 
         let obj = {}
         obj.sessionId = ctx.session.id
-        obj.type = request.body.type
-        obj.name = request.body.name
+        obj.type = ctx.request.body.type
+        obj.name = ctx.request.body.name
         obj.gateId = message.gateId
-        obj.url = 'https://zniesmaczonyzbyszek.herokuapp.com/' + request.body.name
+        obj.url = 'https://zniesmaczonyzbyszek.herokuapp.com/' + ctx.request.body.name
 
         new Message.save(obj)
 
