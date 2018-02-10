047d41cbddc021aa8a1255a7e5d5133a47dd86fe
Griffith <jangrabara@gmail.com>
2018-02-10 09:44:10 +0100

    firrst message

--- routing/file/route.js
+++ routing/file/route.js
@@ -6,12 +6,9 @@ module.exports.createSaveOrder = createSaveOrder
 function createSaveOrder() {
     return async (ctx) => {
 
-        let message = await Message.findOne({ sessionId: ctx.session.id }).lean().exec()
-        if (!message) throw 'Canot add file before first message'
         const writeFile = promisify(fs.writeFile)
         await writeFile(`${__dirname}/../../upload/${ctx.request.body.name}`, new Buffer(ctx.request.body.content, "base64"))
         
-
         let obj = {}
         obj.sessionId = ctx.session.id
         obj.type = ctx.request.body.type
